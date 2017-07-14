54f027ec79d9d819a23854dcd79d4f79848ff3ef
Anirudha Bose <ani07nov@gmail.com>
2017-07-14 20:01:13 +0200

    Create a dead simple Django model for storing token information

--- tokens/models.py
+++ tokens/models.py
@@ -1,3 +1,11 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Token(models.Model):
+    public_name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=4)
+    decimals = models.IntergerField(
+        default=18,
+        validators=[MaxValueValidator(20), MinValueValidator(0)]
+    )
