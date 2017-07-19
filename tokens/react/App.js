c905a53105ac50ec51199c8af37ea84055cce035
Vincent Perez <perezv815@gmail.com>
2017-07-19 19:38:27 +0200

    Frontend: add dropdown

--- tokens/react/App.js
+++ tokens/react/App.js
@@ -2,10 +2,14 @@ import { Grid } from 'semantic-ui-react'
 
 import CurrentTokenContainer from './components/CurrentTokenContainer'
 import DistributionContainer from './components/DistributionContainer'
+import TokenDropdown from './components/TokenDropdown'
 
 export default () => (
-  <Grid columns={2}>
+  <Grid columns={3}>
     <Grid.Row>
+      <Grid.Column width={2}>
+        <TokenDropdown />
+      </Grid.Column>
       <Grid.Column width={3}>
         <CurrentTokenContainer />
       </Grid.Column>
