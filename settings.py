918d7b6380e3a8adf2dbaafe393fcd3866ed714d
Anirudha Bose <ani07nov@gmail.com>
2017-07-23 16:36:13 +0200

    Use cookie-based wallet sessions

--- settings.py
+++ settings.py
@@ -64,6 +64,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
+
 ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
