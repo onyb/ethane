bd3df81d31a51576f58525c33b53f75cf6fc6caa
Anirudha Bose <ani07nov@gmail.com>
2017-07-23 17:32:45 +0200

    Fix imports in urls.py

--- urls.py
+++ urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 from tokens import views as tokenViews
 
